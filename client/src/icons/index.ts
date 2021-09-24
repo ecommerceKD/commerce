6829dfd430a5fda1f81d3de142c956a84f532c8a
Edinelson Lima <edinelsonlima86@gmail.com>
2021-09-24 10:33:56 -0300

    icones do react icons

---
+++ client/src/icons/index.ts
@@ -0,0 +1,24 @@
+export {
+    RiShoppingCart2Line,
+    RiShoppingCart2Fill
+} from 'react-icons/ri'
+
+export {
+    AiOutlineMenu,
+    AiFillHeart,
+    AiOutlineLike,
+    AiOutlineContacts,
+    AiOutlineMail,
+    AiOutlineInstagram,
+    AiOutlineFacebook
+} from 'react-icons/ai'
+
+export {
+    FaShare,
+    FaWhatsapp,
+    FaTelegramPlane
+} from 'react-icons/fa'
+
+export { GoLocation } from 'react-icons/go'
+
+export { BiMenuAltLeft } from 'react-icons/bi'
